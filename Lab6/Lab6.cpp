b6eee54a86d0bfd7fcef725812098b7bb4e9e1d6
664244113-boop <664244113@mail.pbru.ac.th>
2026-06-11 20:22:50 +0700

    Update Lab6.cpp

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -6,57 +6,43 @@
 using namespace std;
 
 const int NUM_PHILOSOPHERS = 5;
-
-mutex chopsticks[NUM_PHILOSOPHERS]; 
-
-mutex cout_mutex; 
+mutex chopsticks[NUM_PHILOSOPHERS];
 
 void philosopher(int id) {
-    int left_chopstick = id;
-    int right_chopstick = (id + 1) % NUM_PHILOSOPHERS;
+    int left = id;
+    int right = (id + 1) % NUM_PHILOSOPHERS;
 
-    int first = (id % 2 == 0) ? right_chopstick : left_chopstick;
-    int second = (id % 2 == 0) ? left_chopstick : right_chopstick;
-
-    for (int i = 0; i < 2; ++i) { 
+    for (int i = 0; i < 2; i++) {
         
-        {
-            lock_guard<mutex> lock(cout_mutex);
-            cout << "Philosopher " << id << " is thinking..." << endl;
-        }
-        this_thread::sleep_for(chrono::milliseconds(500)); 
+        cout << "Philosopher " << id << " is thinking." << endl;
+        this_thread::sleep_for(chrono::milliseconds(100));
 
-        chopsticks[first].lock();
-        chopsticks[second].lock();
-
-        {
-            lock_guard<mutex> lock(cout_mutex);
-            cout << "Philosopher " << id << " is EATING (Round " << i + 1 << ")." << endl;
+        if (id % 2 == 0) {
+            chopsticks[right].lock();
+            chopsticks[left].lock();
+        } else {
+            chopsticks[left].lock();
+            chopsticks[right].lock();
         }
-        this_thread::sleep_for(chrono::milliseconds(500)); 
 
-        chopsticks[first].unlock();
-        chopsticks[second].unlock();
-        
-        {
-            lock_guard<mutex> lock(cout_mutex);
-            cout << "Philosopher " << id << " put down chopsticks." << endl;
-        }
+        cout << "Philosopher " << id << " is eating." << endl;
+        this_thread::sleep_for(chrono::milliseconds(100));
+
+        chopsticks[left].unlock();
+        chopsticks[right].unlock();
     }
 }
 
 int main() {
-    cout << "=== Lab 6: Dining Philosophers Simulation ===" << endl;
     thread philosophers[NUM_PHILOSOPHERS];
 
-    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         philosophers[i] = thread(philosopher, i);
     }
 
-    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         philosophers[i].join();
     }
 
-    cout << "=== All philosophers have finished dining successfully! ===" << endl;
     return 0;
 }
